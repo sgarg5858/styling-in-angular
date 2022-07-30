72dbc2adcdd2c405c0c8970c329953e69f17ca72
Sanjay Garg <sgarg5858@gmail.com>
2022-07-30 18:47:47 +0530

    overriding-child-component-styles-like-a-pro-with-css-variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgDeepInActionComponent } from './ng-deep-in-action/ng-deep-in-action.component';
 import { TestComponent } from './ng-deep-in-action/test/test.component';
+import { ButtonComponent } from './ng-deep-in-action/button/button.component';
+import { CustomButtonComponent } from './custom-button/custom-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgDeepInActionComponent,
-    TestComponent
+    TestComponent,
+    ButtonComponent,
+    CustomButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/custom-button/custom-button.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  templateUrl: './custom-button.component.html',
+  styleUrls: ['./custom-button.component.scss']
+})
+export class CustomButtonComponent implements OnInit {
+
+  @Input() text:string="";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
